f2456879fef8d5b3640e5a553d16353bb88f39d9
Prince <ashnand2000@gmail.com>
2023-08-15 22:49:22 +0530

    Add useState for form to capture value and use it

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
   const initialItems = [
     { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -22,21 +24,46 @@ function App() {
     );
   }
   function Form() {
+    const [description, setDescription] = useState("");
+    const [quantity, setQuantity] = useState(1);
+
     function FormSubmission(e) {
       e.preventDefault();
+
+      if (!description) return;
+
+      const finalList = {
+        description,
+        quantity,
+        packed: false,
+        id: Date.now(),
+      };
+
+      console.log(finalList);
+
+      setDescription("");
+      setQuantity(1);
     }
 
     return (
       <form className="add-form" onSubmit={FormSubmission}>
         <h3>What do you need for your trip?</h3>
-        <select>
+        <select
+          value={quantity}
+          onChange={(val) => setQuantity(val.target.value)}
+        >
           {Array.from({ length: 20 }, (_, i) => i + 1).map((val) => (
             <option value={val} key={val}>
               {val}
             </option>
           ))}
         </select>
-        <input type="text" placeholder="Item..." />
+        <input
+          type="text"
+          placeholder="Item..."
+          value={description}
+          onChange={(msg) => setDescription(msg.target.value)}
+        />
         <button>Add</button>
       </form>
     );
@@ -46,7 +73,7 @@ function App() {
       <div className="list">
         <ul>
           {initialItems.map((item) => (
-            <Item item={item} />
+            <Item item={item} key={item.id} />
           ))}
         </ul>
       </div>
