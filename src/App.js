3ce2815d21e696c500fb47f8a7627703091b72f0
Prince <ashnand2000@gmail.com>
2023-08-15 17:56:07 +0530

    count the date with respect to steps, adding data

--- src/App.js
+++ src/App.js
@@ -1,55 +1,92 @@
 import { useState } from "react";
 
-const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
-];
-
 export default function App() {
-  const [step, setStep] = useState(1);
-  const [isOpen, setOpenClose] = useState(true);
+  const [step, setSteps] = useState(1);
+  const [count, setCount] = useState(0);
 
-  function preciousClicked() {
-    if (step > 1) setStep(step - 1);
+  // const date = new Date("2023-08-25").toLocaleDateString("en-US", {
+  //   weekday: "short",
+  //   year: "numeric",
+  //   month: "short",
+  //   day: "numeric",
+  // });
+
+  // handling + and - date
+  const currentDate = new Date();
+  const futureDate = new Date(currentDate);
+  futureDate.setDate(currentDate.getDate() + count);
+
+  const formattedFutureDate = futureDate.toLocaleDateString("en-US", {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+
+  const style = {
+    backgroundColor: "#7950f2",
+    color: "#ffffff",
+    marginTop: "1rem",
+    textAlign: "center",
+  };
+
+  function decreaseSteps() {
+    if (step > 1) setSteps((s) => s - 1);
   }
 
-  function nextClicked() {
-    if (step < 3) setStep(step + 1);
+  function increaseSteps() {
+    setSteps((s) => s + 1);
   }
+
+  function increaseCount() {
+    setCount((s) => s + step);
+  }
+
+  function decreaseCount() {
+    setCount((s) => s - step);
+  }
+
+  const bodyStyle = {
+    backgroundImage: "url('./back.jpg')", // Replace with the actual path to your image
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    minHeight: "100vh",
+    margin: 0,
+    padding: 0,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
   return (
-    <>
-      <button className="close" onClick={() => setOpenClose(!isOpen)}>
-        &times;
-      </button>
-      {isOpen && (
-        <div className="steps">
-          <div className="numbers">
-            <div className={step >= 1 ? "active" : ""}>1</div>
-            <div className={step >= 2 ? "active" : ""}>2</div>
-            <div className={step >= 3 ? "active" : ""}>3</div>
-          </div>
+    <div style={bodyStyle}>
+      <div className="steps">
+        <h1>Days Counter Web App</h1>
 
-          <p className="message">
-            Step{step} : {messages[step - 1]}
-          </p>
-
-          <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#ffffff" }}
-              onClick={preciousClicked}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#ffffff" }}
-              onClick={nextClicked}
-            >
-              Next
-            </button>
-          </div>
+        <div className="buttons">
+          <button style={style} onClick={decreaseSteps}>
+            Decrease Steps
+          </button>
+          <p>Steps : {step} </p>
+          <button style={style} onClick={increaseSteps}>
+            Increase Steps
+          </button>
         </div>
-      )}
-    </>
+
+        <div className="buttons">
+          <button style={style} onClick={decreaseCount}>
+            Decrease Count
+          </button>
+          <p>Counts : {count} </p>
+          <button style={style} onClick={increaseCount}>
+            Increase Count
+          </button>
+        </div>
+
+        <h4 style={{ textAlign: "center" }}>
+          {count} days from today is {formattedFutureDate}.
+        </h4>
+      </div>
+    </div>
   );
 }
