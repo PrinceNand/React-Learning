7ba6f6e38253b6fc0b65c601e0e9652de65ad8e5
Prince <ashnand2000@gmail.com>
2023-08-25 20:57:48 +0530

    Layout with button and Change of states

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+const bodyStyle = {
+  backgroundImage: "url('./back1.jpg')", // Replace with the actual path to your image
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  minHeight: "100vh",
+  margin: 0,
+  padding: 0,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={bodyStyle}>
+      <Accordion listItem={faqs} />
+    </div>
+  );
+}
+
+function Accordion({ listItem }) {
+  return (
+    <div className="accordion">
+      {/* i is index , el is for loop single item.......below line is used to loop the item and accordianItem shows its using the UI */}
+      {listItem.map((el, i) => (
+        <AccordionItem num={i} title={el.title} text={el.text} />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function onToggle() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={onToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 }
